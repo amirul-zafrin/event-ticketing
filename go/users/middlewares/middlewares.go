c2fd21ada000e7567c2d107ba038a166755d6687
Amirul Zafrin <amirulzafrin98@gmail.com>
2023-11-05 21:19:52 +0800

    Add JWT Deserializer middleware

--- go/users/middlewares/middlewares.go
+++ go/users/middlewares/middlewares.go
@@ -1,12 +1,62 @@
 package middlewares
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/amirul-zafrin/event-ticketing/users.git/config"
+	"github.com/amirul-zafrin/event-ticketing/users.git/database"
+	"github.com/amirul-zafrin/event-ticketing/users.git/models"
+	"github.com/amirul-zafrin/event-ticketing/users.git/routes"
 	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func AuthMiddleware(secret string) fiber.Handler {
 	return jwtware.New(jwtware.Config{
 		SigningKey: jwtware.SigningKey{Key: []byte(secret)},
 	})
+}
+
+func DeserializeUser(c *fiber.Ctx) error {
+	var tokenString string
+	authorization := c.Get("Authorization")
+
+	if strings.HasPrefix(authorization, "Bearer ") {
+		tokenString = strings.TrimPrefix(authorization, "Bearer ")
+	} else if c.Cookies("token") != "" {
+		tokenString = c.Cookies("token")
+	}
+
+	if tokenString == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": "fail", "message": "Please login!"})
+	}
+
+	tokenByte, err := jwt.Parse(tokenString, func(jwtToken *jwt.Token) (interface{}, error) {
+		if _, ok := jwtToken.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %s", jwtToken.Header["alg"])
+		}
+		return []byte(config.Secret), nil
+	})
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(err)
+	}
+
+	claims, ok := tokenByte.Claims.(jwt.MapClaims)
+
+	if !ok || !tokenByte.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": "fail", "message": "invalid token claim"})
+	}
+
+	var user models.User
+	database.Database.Db.First(&user, "id = ?", claims["ID"])
+
+	if user.ID != uint(claims["ID"].(float64)) {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"status": "fail", "message": "the user belonging to this token no logger exists"})
+	}
+
+	c.Locals("user", routes.CreateResponseUser(user))
+	return c.Next()
 }
