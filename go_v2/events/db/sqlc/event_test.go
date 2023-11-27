90fa7739e290f056a191c683798329f1a0329716
Amirul Zafrin <amirulzafrin98@gmail.com>
2023-11-27 22:47:31 +0800

    Add event query testing

--- go_v2/events/db/sqlc/event_test.go
+++ go_v2/events/db/sqlc/event_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func createRandomEvent(t *testing.T) Event {
+func CreateRandomEvent(t *testing.T) Event {
 	arg := CreateEventParams{
 		Name:     util.RandomEventName(),
 		Date:     sql.NullTime{Time: util.RandomDate(), Valid: true},
@@ -32,11 +32,11 @@ func createRandomEvent(t *testing.T) Event {
 	return event
 }
 func TestCreateEvent(t *testing.T) {
-	createRandomEvent(t)
+	CreateRandomEvent(t)
 }
 
-func TestGetAccount(t *testing.T) {
-	event1 := createRandomEvent(t)
+func TestGetEvent(t *testing.T) {
+	event1 := CreateRandomEvent(t)
 	event2, err := testQueries.GetEvent(context.Background(), event1.ID)
 
 	require.NoError(t, err)
@@ -49,8 +49,28 @@ func TestGetAccount(t *testing.T) {
 	require.WithinDuration(t, event1.CreatedAt, event2.CreatedAt, time.Second)
 }
 
-func TestUpdateAccount(t *testing.T) {
-	event1 := createRandomEvent(t)
+func TestListAllEvent(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		CreateRandomEvent(t)
+	}
+
+	arg := ListAllEventsParams{
+		Limit:  5,
+		Offset: 0,
+	}
+
+	events, err := testQueries.ListAllEvents(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, events, 5)
+
+	for _, event := range events {
+		require.NotEmpty(t, event)
+	}
+
+}
+
+func TestUpdateEvent(t *testing.T) {
+	event1 := CreateRandomEvent(t)
 
 	arg := UpdateEventParams{
 		ID:   event1.ID,
@@ -59,10 +79,32 @@ func TestUpdateAccount(t *testing.T) {
 	event2, err := testQueries.UpdateEvent(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, event2)
+	require.True(t, event2.UpdatedAt.Valid)
 
 	require.Equal(t, arg.Name, event2.Name)
 	require.Equal(t, event1.Location, event1.Location)
 	require.Equal(t, event1.Date, event2.Date)
 
 	require.WithinDuration(t, event1.CreatedAt, event2.CreatedAt, time.Second)
+}
+
+func TestSoftDelete(t *testing.T) {
+	event := CreateRandomEvent(t)
+
+	testQueries.SoftDeleteEvent(context.Background(), event.ID)
+
+	event1, err := testQueries.GetEvent(context.Background(), event.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, event1.Name)
+	require.NotEmpty(t, event1.DeletedAt)
+}
+
+func TestPermaDelete(t *testing.T) {
+	event := CreateRandomEvent(t)
+
+	testQueries.PermaDeleteEvent(context.Background(), event.ID)
+
+	_, err := testQueries.GetEvent(context.Background(), event.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
 }
